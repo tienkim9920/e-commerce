6b18ea67bec05e94d510a3472d6417ccd99988ad
tienkim9920 <tienkim9920@gmail.com>
2021-09-21 19:43:05 +0700

    e-commerce

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeRoutingModule } from './home-routing.module';
+import { IndexComponent } from './index/index.component';
+
+
+@NgModule({
+  declarations: [
+    IndexComponent
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule
+  ]
+})
+export class HomeModule { }
