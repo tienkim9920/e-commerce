9c6167c5f18fa59650ae3fe620b792b5f6b2002c
tienkim9920 <tienkim9920@gmail.com>
2021-09-25 16:32:16 +0700

    update

--- src/app/home/index/index.component.ts
+++ src/app/home/index/index.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-index',
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('widgetsContent', { read: ElementRef }) widgetsContent!: ElementRef<any>;
+
+  scroll: any = true
+
+  constructor() {
+
+  }
 
   ngOnInit(): void {
+  }
+
+  public scrollLeft(): void {
+    this.widgetsContent.nativeElement.scrollTo({ left: (this.widgetsContent.nativeElement.scrollLeft - 1250), behavior: 'smooth' });
+    this.scroll = !this.scroll
+  }
+
+  public scrollRight(): void {
+    this.widgetsContent.nativeElement.scrollTo({ left: (this.widgetsContent.nativeElement.scrollLeft + 1250), behavior: 'smooth' });
+    this.scroll = !this.scroll
   }
 
 }
