877dbf06b2348e0dfcc6f402f657f713f7089c98
toannguyen197 <hahadaubo@gmail.com>
2021-10-10 15:02:55 +0700

    Update pull

---
+++ src/app/manages/datacenter/addcoupon/addcoupon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addcoupon',
+  templateUrl: './addcoupon.component.html',
+  styleUrls: ['./addcoupon.component.css']
+})
+export class AddcouponComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/manages/message/message.component.ts
+++ src/app/manages/message/message.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-message',
@@ -7,9 +7,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessageComponent implements OnInit {
 
+  @ViewChild('scrollbottom', { read: ElementRef }) scrollBottom!: ElementRef<any>;
+
+  message: any
+
+  listMessage: any = [
+    { message: 'Hé lô', category: 'send'},
+    { message: 'Hé lô', category: 'receive'},
+    { message: 'Hé lô', category: 'send'},
+    { message: 'Hé lô', category: 'receive'},
+    { message: 'Hé lô', category: 'send'},
+    { message: 'Hé lô', category: 'receive'},
+    { message: 'Hé lô', category: 'send'},
+  ]
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    setTimeout(() => {
+      this.scrollToBottom()
+    }, 10)
+  }
+
+  scrollToBottom() {
+    this.scrollBottom.nativeElement.scrollTop = this.scrollBottom.nativeElement.scrollHeight;
+  }
+
+  sendMessage(){
+
+    const data = {
+      message: this.message,
+      category: 'send'
+    }
+
+    const newMessages = this.listMessage
+    newMessages.push(data)
+
+    this.listMessage = newMessages
+
+    setTimeout(() => {
+      this.scrollToBottom()
+    }, 10)
+
+    this.message = ''
+  }
+
+  onEnter(){
+    this.sendMessage()
   }
 
 }
