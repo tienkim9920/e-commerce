0727a963b8d6d0332d9f65b26cb496eb52ad40ee
toannguyen197 <hahadaubo@gmail.com>
2021-10-19 16:32:47 +0700

    pull

---
+++ src/app/pattern/Category.ts
@@ -0,0 +1,13 @@
+class Category{
+
+  _id: any
+  name: any
+  product: any = []
+
+  constructor (_id: any){
+    this._id = _id
+  }
+
+}
+
+export default Category
---
+++ src/app/pattern/Tick.ts
@@ -0,0 +1,13 @@
+class Tick{
+
+  _id: any
+  name: any
+  description: any
+  value: any
+  ticket: any = []
+
+  constructor (_id: any){
+    this._id = _id
+  }
+}
+export default Tick
