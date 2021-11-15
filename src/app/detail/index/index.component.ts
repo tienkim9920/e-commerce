1d7399ed697c00cadda371f81232a385f16d0041
toannguyen197 <hahadaubo@gmail.com>
2021-11-15 16:22:52 +0700

    push

--- src/app/detail/index/index.component.ts
+++ src/app/detail/index/index.component.ts
@@ -33,7 +33,7 @@ export class IndexComponent implements OnInit {
 
   index = 0
 
-  product = new Product('', '', '', '', '', '', [],'' , '', '', '')
+  product = new Product('', '', '', '', '', '', [], '', '', '', '')
 
   message: string = ''
 
